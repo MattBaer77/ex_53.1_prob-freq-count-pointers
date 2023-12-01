ff92b9b8dfd258bc3ee79e6eca9cf8aa701d62e2
Matthew Baer <matt.baer77@gmail.com>
2023-11-30 23:56:43 -0500

    completed separate-positive

--- separate-positive.js
+++ separate-positive.js
@@ -16,7 +16,50 @@ Constraints
 Time Complexity: O(N)
 
 */
-function separatePositive() {
+
+/* Questions - can I save any variables?
+
+can I save a right and left array and put them together at the end / re-add them to the original array? (Empty out the original and fill it back up?)
+can I pop numbers from the end and then add them back into the array in order?
+
+going to assume I cannot do these and so I will take the approach of swapping indexes
+
+this is a two pointers problem
+
+*/
+
+function separatePositive(arr) {
+
+    // separate positive integers to left and negative integers to right
+    // do in place - do not build a copy of the input array
+
+    // two pointers toward the middle - swap assign indexes if conditional shows negative on left and positive on right swap
+    // if left is > 0 left++
+    // else right -- to keep comparisons moving
+
+    let left = 0;
+    let right = arr.length -1;
+
+    while (left < right){
+
+        if(arr[left] < 0 && arr[right] > 0){
+
+            [arr[left], arr[right]] = [arr[right], arr[left]]
+
+        } else if(arr[left] > 0) {
+
+            left ++
+
+        } else {
+
+            right --
+
+        }
+
+    }
+
+    return arr
+
 }
 
 module.exports=separatePositive
